0c16ca4e48e71c15e917e4f7f7469c818aaef743
Svyatoslav Kosovskikh <svyatoslav_kosovskikh@epam.com>
2021-02-11 20:38:44 +0300

    Headers collection scripts

---
+++ concordance_pandas.py
@@ -0,0 +1,67 @@
+import string
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize
+import pandas as pd
+from datetime import datetime
+
+stemmer = WordNetLemmatizer()
+stoplist = pd.Series('for a of or be with the at and to in is if on no not an it who'.split())
+
+
+def read_from_file(file):
+    with open(file, 'r') as f:
+        cells = f.readlines()
+    return [cell.replace('\n', ' ').replace('  ', ' ') for cell in cells]
+
+
+def translate(word) -> str:
+    return word.translate(str.maketrans('', '', string.punctuation)).strip()
+
+
+def count_probability(df: pd.DataFrame) -> pd.DataFrame:
+    # Create DataFrame consisting of words
+    word_sequence = df['words'].map(
+        lambda x: word_tokenize(x.lower())).explode().apply(translate)
+
+    # Remove NaN
+    word_sequence = word_sequence[word_sequence.astype(bool)]
+    # Lemmatize
+    word_sequence = word_sequence.apply(lambda x: stemmer.lemmatize(x))
+
+    final_df = word_sequence.to_frame()
+    # Calculate word's count and probability
+    final_df['counts'] = final_df['words']\
+        .map(final_df['words'].value_counts())
+    final_df['probability'] = final_df['words']\
+        .map(final_df['words'].value_counts(normalize=True))
+
+    # final_df.reset_index(drop=True, inplace=True)
+    # Drop stop list words
+    final_df = final_df[~final_df['words'].isin(stoplist)]
+    final_df.set_index('words', inplace=True)
+
+    # TODO: change count logic
+    final_df = final_df[~final_df.index.duplicated()]
+    return final_df
+
+
+def run_lemmatizer(name):
+    """
+    @param name: name of the cell type
+    """
+
+    sequence = read_from_file(f'{name}.txt')
+    df = pd.DataFrame(sequence, columns=['words'])
+    df = count_probability(df)
+    df.rename_axis(index={'words': f'{name}'}, inplace=True)
+    # df.to_json(f'{name}.json', orient='index')
+    return df
+
+
+start = datetime.now()
+dfh = run_lemmatizer('headers')
+# Drop digits in headers
+dfh = dfh[~dfh['words'].str.isdigit()]
+dfc = run_lemmatizer('cells')
+
+print(datetime.now() - start)
