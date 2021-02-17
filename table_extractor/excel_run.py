62add4c9dfb84d2bcc939d8355a1851960ace6d5
Matvei Vargasov <matveyvarg@gmail.com>
2021-02-17 16:34:28 +0300

    cli added

---
+++ table_extractor/excel_run.py
@@ -0,0 +1,14 @@
+import click
+
+from table_extractor.excel_extractor import run_excel_job
+
+
+@click.command()
+@click.argument('file')
+@click.argument('output')
+def run(file: str, output: str):
+    run_excel_job(file, output)
+
+
+if __name__ == "__main__":
+    run()
