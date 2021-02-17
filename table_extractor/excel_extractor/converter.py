cd7d359f5745f0fa3f5f60ff9309b07959af7912
Matvei Vargasov <matveyvarg@gmail.com>
2021-02-17 16:20:07 +0300

    write file

--- table_extractor/excel_extractor/converter.py
+++ table_extractor/excel_extractor/converter.py
@@ -6,6 +6,9 @@ from table_extractor.model.table import (
     CellLinked,
     TextField
 )
+from table_extractor.headers.header_utils import HeaderChecker
+
+header_checker = HeaderChecker()
 
 
 def convert_cells(cells: dict) -> list:
@@ -37,12 +40,13 @@ def convert_cells(cells: dict) -> list:
     return converted_cells
 
 
-def excel_to_structured(excel_tables_sheets: dict) -> List[StructuredTable]:
+def excel_to_structured(excel_tables_sheets: dict) -> dict:
     """
     Converts data from excel to structured table
     """
-    tables = []
-    for sheet_tables in excel_tables_sheets.values():
+    tables = {}
+    for sheet, sheet_tables in excel_tables_sheets.items():
+        tables.setdefault(sheet, [])
         for excel_table in sheet_tables:
             print(excel_table['dimensions'])
             table = StructuredTable(
@@ -54,6 +58,54 @@ def excel_to_structured(excel_tables_sheets: dict) -> List[StructuredTable]:
                     excel_table['dimensions'][1]['bottom_right'][1],
                 )
             )
-            tables.append(table)
+            tables[sheet].append(table)
 
     return tables
+
+
+def create_header(series: List[List[CellLinked]], header_limit: int):
+    """
+    Search for headers based on cells contents
+    @param series: cols or rows of the table
+    @param table:
+    @param header_limit:
+    """
+    header_candidates = []
+    last_header = None
+    for idx, line in enumerate(series[:header_limit]):
+        if analyse(line):
+            header_candidates.append((idx, True, line))
+            last_header = idx
+        else:
+            header_candidates.append((idx, False, line))
+    if last_header is not None:
+        header = [line for idx, is_header, line in header_candidates[:last_header + 1]]
+    else:
+        header = []
+    if len(header) > 0.75 * len(series):
+        header = []
+    return header
+
+
+def analyse(series: List[CellLinked]):
+    # Check if series is header
+    headers = []
+    for cell in series:
+        header_score, _ = header_checker.get_cell_score(cell)
+        if header_score > 0:
+            headers.append(cell)
+    return len(headers) > (len(series) / 5) if len(series) > 5 else len(headers) > (len(series) / 2)
+
+
+def get_headers(tables: dict) -> dict:
+    tables_with_header = {}
+    for sheet, sheet_tables in tables.items():
+        tables_with_header.setdefault(sheet, [])
+        for table in sheet_tables:
+            header_rows = create_header(table.rows, 6)
+            table_with_header = StructuredTableHeadered.from_structured_and_rows(table, header_rows)
+            header_cols = create_header(table.cols, 4)
+            table_with_header.actualize_header_with_cols(header_cols)
+            tables_with_header[sheet].append(table_with_header)
+
+    return tables_with_header
---
+++ table_extractor/excel_extractor/writer.py
@@ -0,0 +1,54 @@
+from typing import List, Dict
+from dataclasses import dataclass
+
+from table_extractor.model.table import StructuredTableHeadered
+
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+
+
+class BaseWriter:
+    """
+    Base class for writers
+    """
+
+    def __init__(self, data: Dict[str, list], outpath: str):
+        self.data = data
+        self.outpath = outpath
+
+    def write(self):
+        raise NotImplemented
+
+
+class ExcelWriter(BaseWriter):
+    """
+    Generate excel from structured table
+    """
+    wb = Workbook()
+
+    def write(self):
+        header_font = Font(bold=True)
+        header_fill = PatternFill(start_color="00C0C0C0", end_color="00C0C0C0", fill_type = "solid")
+        header_border = thin_border = Border(left=Side(style='thick'),
+                     right=Side(style='thick'),
+                     top=Side(style='thick'),
+                     bottom=Side(style='thick'))
+
+        for i, (sheet, tables) in enumerate(self.data):
+            print(i, sheet)
+            if not i:
+                ws = self.wb.active
+            else:
+                ws = self.wb.create_sheet(sheet)
+
+            for table in tables:
+                for header_cells in table.header:
+                    for cell in header_cells:
+                        added_cell = ws.cell(row=cell.row, column=cell.col, value=cell.text_boxes[0].text)
+                        added_cell.fill = header_fill
+                        added_cell.font = header_font
+
+                for cell in table.cells:
+                    ws.cell(row=cell.row, column=cell.col, value=cell.text_boxes[0].text)
+
+        self.wb.save('ex.xlsx')
